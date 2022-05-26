1ea66c99ff737149668bbb4d043f291e43b5a2dd
aadiaconitei <email@address.com>
2022-05-26 20:57:58 +0300

    curs8

--- curs7/mycrud/backend/server.ts
+++ curs7/mycrud/backend/server.ts
@@ -1,11 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { userRouter } from './routes/userRouter';
 
 dotenv.config();
 
 const app: Express = express();
+app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT;
-
+app.use(cors());
+app.use("/users", userRouter);
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
